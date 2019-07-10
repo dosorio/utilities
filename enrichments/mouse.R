1186c395be4012bbe60464f011e03def4ebb3161
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-10 11:44:34 -0500

    Create mouse.R

---
+++ enrichments/mouse.R
@@ -0,0 +1,19 @@
+makeMouseEnrichmentGO <- function(X){
+  require(clusterProfiler)
+  require(AnnotationHub)
+  db <- AnnotationHub()
+  mmu <- db[["AH70573"]]
+  eIds <- select(mmu,X, "ENTREZID", "SYMBOL")[,2]
+  eOut <- clusterProfiler::enrichGO(gene = eIds, keyType = "ENTREZID", OrgDb = mmu)
+  as.data.frame(eOut)
+}
+
+makeMouseEnrichmentKEGG <- function(X){
+  require(clusterProfiler)
+  require(AnnotationHub)
+  db <- AnnotationHub()
+  mmu <- db[["AH70573"]]
+  eIds <- select(mmu,X, "ENTREZID", "SYMBOL")[,2]
+  eOut <- clusterProfiler::enrichKEGG(gene = eIds, keyType = "ENTREZID", OrgDb = mmu)
+  as.data.frame(eOut)
+}
