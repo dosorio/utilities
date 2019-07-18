94939b25bce0dceff7c392ed559089402f6ee20e
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-18 12:30:14 -0500

    Update makeConsensusNet.R

--- graphs/makeConsensusNet.R
+++ graphs/makeConsensusNet.R
@@ -4,7 +4,7 @@ makeConsensusNet <- function(fileList, threshold=0.99){
     rNet <- read.csv(X, header = TRUE, row.names = 1)
     diag(rNet) <- NA
     thresholdValue <- quantile(abs(rNet), threshold, na.rm = TRUE)
-    rNet[isTRUE(rNet < thresholdValue)] <- NA
+    rNet[rNet < thresholdValue] <- NA
     rNet[upper.tri(rNet, diag = TRUE)] <- NA
     rNet <- reshape2::melt(as.matrix(rNet))
     rNet <- rNet[complete.cases(rNet),]
