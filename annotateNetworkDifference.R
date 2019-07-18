773a8db72e5901f39f86dd0675bbe0b6a6071fb2
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-18 08:55:22 -0500

    Create annotateNetworkDifference.R

---
+++ annotateNetworkDifference.R
@@ -0,0 +1,27 @@
+annotateNetworkDifference <- function(X, Y, type="KEGG"){
+  source("https://raw.githubusercontent.com/dosorio/utilities/master/enrichments/mouse.R")
+  if(type == "KEGG"){
+    D <- difference(X,Y)
+    louvainClusters <- cluster_louvain(D)
+    annotationKEGG <- lapply(unique(louvainClusters$membership), function(C){
+      out <- try(makeMouseEnrichmentKEGG(G1$names[louvainClusters$membership %in% C]), silent = TRUE)
+      if(class(out) != "try-error"){return(out)}
+    })
+    annotationKEGG <- do.call(rbind.data.frame, annotationKEGG)
+    annotationKEGG <- annotationKEGG[order(annotationKEGG$p.adjust),]
+    rownames(annotationKEGG) <- NULL
+    return(annotationKEGG)
+  }
+  if(type == "GO"){
+    D <- difference(X,Y)
+    louvainClusters <- cluster_louvain(D)
+    annotationGO <- lapply(unique(louvainClusters$membership), function(C){
+      out <- try(makeMouseEnrichmentGO(G1$names[louvainClusters$membership %in% C]), silent = TRUE)
+      if(class(out) != "try-error"){return(out)}
+    })
+    annotationGO <- do.call(rbind.data.frame, annotationGO)
+    annotationGO <- annotationGO[order(annotationGO$p.adjust),]
+    rownames(annotationGO) <- NULL
+    return(annotationGO)
+  }
+}
