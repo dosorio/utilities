4ca1ba80cd33bfbf65efa6034bc69808f439266d
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-19 14:25:16 -0500

    Update pcNet.R

--- singleCell/pcNet.R
+++ singleCell/pcNet.R
@@ -1,24 +1,37 @@
 pcNet <- function(X, nCom = 3, nCores = 5){
-  require(parallel)
   require(RSpectra)
-  cl <- makeCluster(getOption("cl.cores", nCores))
   gNames <- rownames(X)
-  X <- scale(t(X))
+  X <- (scale(t(X)))
   n <- ncol(X)
   A <- 1-diag(n)
-  clusterExport(cl,"X", envir = environment())
-  clusterExport(cl,"nCom", envir = environment())
-  B <- parSapply(cl, seq_len(n), function(K){
-    y <- X[,K]
-    Xi <- X
-    Xi <- Xi[,-K]
-    coeff <- RSpectra::svds(Xi, nCom)$v
-    score <- Xi %*% coeff
-    score <- t(t(score)/(apply(score,2,function(X){sqrt(sum(X^2))})^2))
-    Beta <- colSums(y * score)
-    return(coeff %*% (Beta))
-  })
-  stopCluster(cl)
+  if(nCores > 1){
+    require(parallel)
+    cl <- makeCluster(getOption("cl.cores", nCores))
+    clusterExport(cl,"X", envir = environment())
+    clusterExport(cl,"nCom", envir = environment())
+    B <- parSapply(cl, seq_len(n), function(K){
+      y <- X[,K]
+      Xi <- X
+      Xi <- Xi[,-K]
+      coeff <- RSpectra::svds(Xi, nCom)$v
+      score <- Xi %*% coeff
+      score <- t(t(score)/(apply(score,2,function(X){sqrt(sum(X^2))})^2))
+      Beta <- colSums(y * score)
+      return(coeff %*% (Beta))
+    })
+    stopCluster(cl)
+  } else {
+    B <- sapply(seq_len(n), function(K){
+      y <- X[,K]
+      Xi <- X
+      Xi <- Xi[,-K]
+      coeff <- RSpectra::svds(Xi, nCom)$v
+      score <- Xi %*% coeff
+      score <- t(t(score)/(apply(score,2,function(X){sqrt(sum(X^2))})^2))
+      Beta <- colSums(y * score)
+      return(coeff %*% (Beta))
+    })
+  }
   B <- t(B)
   for(K in seq_len(n)){
     A[K,A[K,] == 1] = B[K,]
