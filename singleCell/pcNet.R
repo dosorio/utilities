22415a743fea74d23a9ec47fe11f3d231665ff61
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-19 13:44:36 -0500

    Create pcNet.R

---
+++ singleCell/pcNet.R
@@ -0,0 +1,24 @@
+pcNet <- function(X, nCom = 3, nCores = 1){
+  gNames <- rownames(X)
+  X <- (scale(t(X)))
+  n <- ncol(X)
+  A <- 1-diag(n)
+  B <- pbapply::pbsapply(seq_len(n), function(K){
+    y <- X[,K]
+    Xi <- X
+    Xi <- Xi[,-K]
+    coeff <- svds(Xi, nCom)$v
+    score <- Xi %*% coeff
+    score <- t(t(score)/(apply(score,2,function(X){sqrt(sum(X^2))})^2))
+    Beta <- colSums(y * score)
+    return(coeff %*% (Beta))
+  })
+  B <- t(B)
+  for(K in seq_len(n)){
+    A[K,A[K,] == 1] = B[K,]
+  }
+  diag(A) <- 1
+  A <- round(A,5)
+  colnames(A) <- rownames(A) <- gNames
+  return(A)
+}
