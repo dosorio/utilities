17e4d24e916699f2b27a9f1787d1cf46c8d61cf5
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-22 09:45:32 -0500

    Create makeGraph.R

---
+++ singleCell/makeGraph.R
@@ -0,0 +1,9 @@
+  makeGraph <- function(X, qThreshold = 0.9){
+    diag(X) <- NA
+    qT <- quantile(abs(X), qThreshold, na.rm = TRUE)
+    X[abs(X) < qT] <- NA
+    X <- reshape2::melt(X)
+    X <- X[complete.cases(X), ]
+    X <- igraph::graph_from_data_frame(X)
+    return(X)
+  }
