5b9aab1720fe64c2a391dcd47052c8fbe8d40e6b
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-22 10:08:48 -0500

    Update makeBnet.R

--- singleCell/makeBnet.R
+++ singleCell/makeBnet.R
@@ -1,7 +1,7 @@
   makeBnet <- function(X, qThreshold = 0.9, nCell = 1000, nCom = 3){
     cellNames <- colnames(X)
     bX <- X[, sample(cellNames, nCell, replace = TRUE)]
-    bX <- bX[rowMeans(bX != 0) > 0.05, ]
+    bX <- bX[rowMeans(bX != 0) > 0, ]
     oN <- pcNet(bX, nCom = nCom)
     oN <- makeGraph(oN, qThreshold = qThreshold)
     return(oN)
