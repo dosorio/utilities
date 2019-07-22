6193dcd31f9684a4b48ee79d63843443a81c2ac5
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-22 09:01:59 -0500

    Update pcNet.R

--- singleCell/pcNet.R
+++ singleCell/pcNet.R
@@ -1,51 +1,25 @@
-pcNet <- function(X, nCom = 3, nCores = 1){
+pcNet <- function(X, nCom = 3){
   gNames <- rownames(X)
   X <- (scale(t(X)))
   n <- ncol(X)
   A <- 1-diag(n)
-  if(nCores > 1){
-    if(grepl("Windows", Sys.info()[[1]])){
-      cl <- parallel::makeCluster(getOption("cl.cores", nCores))
-    } else {
-      cl <- parallel::makeCluster(getOption("cl.cores", nCores),  type = "FORK")
+  getCoefficients <- function(K){
+    y <- X[,K]
+    Xi <- X
+    Xi <- Xi[,-K]
+    coeff <- RSpectra::svds(Xi, nCom)$v
+    if(class(coeff) != 'try-error'){
+      score <- Xi %*% coeff
+      score <- t(t(score)/(apply(score,2,function(X){sqrt(sum(X^2))})^2))
+      Beta <- colSums(y * score)
+      Beta <- coeff %*% (Beta)
+      Beta <- round(Beta,5)
+      return(Beta)
+    } else{
+      return(rep(NA, ncol(Xi)))
     }
-    parallel::clusterExport(cl,"X", envir = environment())
-    parallel::clusterExport(cl,"nCom", envir = environment())
-    B <- pbapply::pbsapply(seq_len(n), function(K){
-      y <- X[,K]
-      Xi <- X
-      Xi <- Xi[,-K]
-      coeff <- RSpectra::svds(Xi, nCom)$v
-      if(class(coeff) != 'try-error'){
-        score <- Xi %*% coeff
-        score <- t(t(score)/(apply(score,2,function(X){sqrt(sum(X^2))})^2))
-        Beta <- colSums(y * score)
-        Beta <- coeff %*% (Beta)
-        Beta <- round(Beta,5)
-        return(Beta)
-      } else{
-        return(rep(NA, ncol(Xi)))
-      }
-    }, cl = cl)
-    parallel::stopCluster(cl)
-  } else {
-    B <- pbapply::pbsapply(seq_len(n), function(K){
-      y <- X[,K]
-      Xi <- X
-      Xi <- Xi[,-K]
-      coeff <- try(RSpectra::svds(Xi, nCom)$v, silent = TRUE)
-      if(class(coeff) != 'try-error'){
-        score <- Xi %*% coeff
-        score <- t(t(score)/(apply(score,2,function(X){sqrt(sum(X^2))})^2))
-        Beta <- colSums(y * score)
-        Beta <- coeff %*% (Beta)
-        Beta <- round(Beta,5)
-        return(Beta)
-      } else{
-        return(rep(NA, ncol(Xi)))
-      }
-    })
   }
+  B <- pbapply::pbsapply(seq_len(n), getCoefficients)
   B <- t(B)
   for(K in seq_len(n)){
     A[K,A[K,] == 1] = B[K,]
