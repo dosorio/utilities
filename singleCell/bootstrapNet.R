bfd58d1cc27d4e904b048634d255aed065293da1
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-22 10:07:37 -0500

    Update bootstrapNet.R

--- singleCell/bootstrapNet.R
+++ singleCell/bootstrapNet.R
@@ -3,12 +3,14 @@ bootstrapNet <- function(X, B = 100, qThreshold = 0.9, nCell = 1000, nCom = 3) {
   source("https://raw.githubusercontent.com/dosorio/utilities/master/singleCell/makeGraph.R")
   oN <- pcNet(X, nCom)
   oN <- makeGraph(oN, qThreshold = qThreshold)
-  sapply(seq_len((B)), function(b){
-    nN <- try(makeBnet(X, nCell, nCom), silent = TRUE)
+  b <- 0
+  while (b < B) {
+    nN <- try(makeBnet(X, qThreshold = qThreshold, nCell, nCom), silent = TRUE)
     if (class(nN) == 'igraph') {
-      oN <<- igraph::intersection(oN, nN, keep.all.vertices = FALSE)
+      oN <- igraph::intersection(oN, nN, keep.all.vertices = FALSE)
+      b <- b + 1
     }
-  })
+  }
   oN <- oN[,]
   oN[oN != 0] <- 1
   oN <- oN[apply(oN,1,sum) > 0, apply(oN,2,sum)]
