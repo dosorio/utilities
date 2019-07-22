7e0e5eefb4a4e4f690e0b77bcd2d101aa6f4486a
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-22 13:57:57 -0500

    Update hsa_GO_ENTREZ.R

--- enrichments/hsa_GO_ENTREZ.R
+++ enrichments/hsa_GO_ENTREZ.R
@@ -1,6 +1,8 @@
 hsa_GO_ENTREZ <- function(X){
   require(clusterProfiler)
   require(AnnotationHub)
+  db <- AnnotationHub()
+  hsa <- db[["AH70572"]
   out <- clusterProfiler::enrichGO(gene = X, keyType = "ENTREZID", OrgDb = hsa)
   as.data.frame(out)
 }
