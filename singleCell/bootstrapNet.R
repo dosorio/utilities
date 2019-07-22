ee86cf24b3d71cec10363e7aff3a453b2208ec29
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-22 09:37:48 -0500

    Update bootstrapNet.R

--- singleCell/bootstrapNet.R
+++ singleCell/bootstrapNet.R
@@ -1,25 +1,31 @@
 bootstrapNet <- function(X, B = 100, nCell = 1000, nCom = 3) {
+  makeGraph <- function(X){
+    diag(X) <- NA
+    qT <- quantile(abs(X), 0.9, na.rm = TRUE)
+    X[abs(X) < qT] <- NA
+    X <- reshape2::melt(X)
+    X <- X[complete.cases(X), ]
+    X <- igraph::graph_from_data_frame(X)
+    return(X)
+  }
   makeBnet <- function(X, nCell = 1000, nCom = 3){
     cellNames <- colnames(X)
     bX <- X[, sample(cellNames, nCell, replace = TRUE)]
     bX <- bX[rowMeans(bX != 0) > 0.05, ]
     oN <- pcNet(bX, nCom = nCom)
-    diag(oN) <- NA
-    qT <- quantile(abs(oN), 0.9, na.rm = TRUE)
-    oN[abs(oN) < qT] <- NA
-    oN <- reshape2::melt(oN)
-    oN <- oN[complete.cases(oN), ]
-    oN <- igraph::graph_from_data_frame(oN)
+    oN <- makeGraph(oN)
     return(oN)
   }
-  oN <- makeBnet(X, nCell, nCom)
-  sapply(seq_len((B-1)), function(b){
+  oN <- pcNet(X, nCom)
+  oN <- makeGraph(oN)
+  sapply(seq_len((B)), function(b){
     nN <- try(makeBnet(X, nCell, nCom), silent = TRUE)
     if (class(nN) == 'igraph') {
       oN <<- igraph::intersection(oN, nN, keep.all.vertices = FALSE)
     }
   })
   oN <- oN[,]
+  oN[oN != 0] <- 1
   oN <- oN[apply(oN,1,sum) > 0, apply(oN,2,sum)]
   oN <- reshape2::melt(as.matrix(oN))
   oN <- oN[oN[,3] != 0,]
