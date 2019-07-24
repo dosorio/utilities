6d4fe21f9ea92e1f161bed31a0f201e20298ce47
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-24 10:52:32 -0500

    Update makeBnet.R

--- singleCell/makeBnet.R
+++ singleCell/makeBnet.R
@@ -1,4 +1,5 @@
   makeBnet <- function(X, qThreshold = 0.9, nCell = 1000, nCom = 3){
+    source("https://raw.githubusercontent.com/dosorio/utilities/master/singleCell/pcNet.R")
     cellNames <- colnames(X)
     bX <- X[, sample(cellNames, nCell, replace = TRUE)]
     bX <- bX[rowMeans(bX != 0) > 0, ]
