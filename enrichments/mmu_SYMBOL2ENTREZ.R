9c56401bd4a193384c3dbb60af8e42ba944e4098
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-25 09:02:26 -0500

    Delete mmu_SYMBOL2ENTREZ.R

--- enrichments/mmu_SYMBOL2ENTREZ.R
+++
@@ -1,9 +0,0 @@
-mmu_SYMBOL2ENTREZ <- function(X){
-  require(AnnotationHub)
-  require(AnnotationDbi)
-  db <- AnnotationHub()
-  mmu <- db[["AH70573"]]
-  eIds <- select(mmu,X, "ENTREZID", "SYMBOL")
-  eIds <- eIds[complete.cases(eIds),]
-  return(eIds)
-}
