20533e14585898fcee0c01704efa58a3289fa33f
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-26 12:33:02 -0500

    Create gSets_KEGG.R

---
+++ enrichments/gSets_KEGG.R
@@ -0,0 +1,15 @@
+gSets_KEGG <- function(sp){
+  keggSP <- keggList("pathway",sp)
+  keggSP <- as.data.frame(keggSP)
+  gSetsSP <- lapply(rownames(keggSP), function(X){
+    X <- try(keggLink(X)[,2], silent = TRUE)
+    if(class(X)!="try-error"){
+      X <- X[grepl(paste0(sp,":"),X)]
+      return(gsub(paste0(sp,":"),"",X) )
+    } else{
+      return(NA)
+    }
+  })
+  names(gSetsSP) <- as.character(keggSP[,1])
+  return(gSetsSP)
+}
