25ca69784fa9648b2c961062d76e746ec18d1876
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-29 13:00:44 -0500

    Update bootstrapNet.R

--- singleCell/bootstrapNet.R
+++ singleCell/bootstrapNet.R
@@ -1,6 +1,7 @@
 bootstrapNet <- function(X, B = 100, qThreshold = 0.9, nCell = 1000, nCom = 3) {
   source("https://raw.githubusercontent.com/dosorio/utilities/master/singleCell/makeBnet.R")
   source("https://raw.githubusercontent.com/dosorio/utilities/master/singleCell/makeGraph.R")
+  source("https://raw.githubusercontent.com/dosorio/utilities/master/singleCell/cleanNet.R")
   oN <- pcNet(X, nCom)
   oN <- makeGraph(oN, qThreshold = qThreshold)
   b <- 0
@@ -11,10 +12,6 @@ bootstrapNet <- function(X, B = 100, qThreshold = 0.9, nCell = 1000, nCom = 3) {
       b <- b + 1
     }
   }
-  oN <- as.matrix(oN[,])
-  oN <- oN[apply(oN,1,sum) > 0, apply(oN,2,sum)> 0]
-  oN <- reshape2::melt(as.matrix(oN))
-  oN <- oN[oN[,3] != 0,]
-  oN <- igraph::graph_from_data_frame(oN)
+  oN <- cleanNet(oN)
   return(oN)
 }
