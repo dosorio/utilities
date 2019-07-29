e36b76b93dd725a9f2ed0d9d277e355192b53f4f
Daniel Osorio <dosorio@users.noreply.github.com>
2019-07-29 15:45:48 -0500

    Update hsa_ENSEMBL2SYMBOL.R

--- idConvert/hsa_ENSEMBL2SYMBOL.R
+++ idConvert/hsa_ENSEMBL2SYMBOL.R
@@ -2,8 +2,8 @@ hsa_ENSEMBL2SYMBOL <- function(X){
   require(AnnotationHub)
   require(AnnotationDbi)
   db <- AnnotationHub()
-  hsa <- db[["AH70572"]]
-  eIds <- select(hsa,X, "SYMBOL", "ENSEMBL")
-  eIds <- eIds[complete.cases(eIds),]
+  hsa <- db[["AH73881"]]
+  eIds <- select(hsa,X, "SYMBOL", "GENEID")
+  eIds <- eIds[eIds[,2] != "",]
   return(eIds)
 }
