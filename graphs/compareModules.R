0b78ff0bd256674cf1919135046bb17dcba16624
Daniel Osorio <dosorio@users.noreply.github.com>
2019-08-02 11:46:31 -0500

    Create compareModules.R

---
+++ graphs/compareModules.R
@@ -0,0 +1,18 @@
+compareModules <- function(netX, netY){
+  cX <- cluster_walktrap(netX, steps = 100)
+  cY <- cluster_walktrap(netY, steps = 100)
+  
+  S <- sapply(unique(cX$membership), function(mX){
+    sapply(unique(cY$membership), function(mY){
+      gX <- cX$names[cX$membership == mX]
+      gY <- cY$names[cY$membership == mY]
+      allG <- unique(c(gX,gY))
+      sMatrix <- matrix(FALSE, nrow = length(allG), ncol = 2)
+      rownames(sMatrix) <- allG
+      sMatrix[gX,1] <- TRUE
+      sMatrix[gY,2] <- TRUE
+      round(mean(apply(sMatrix,1,all)),2)
+    })
+  })
+  return(S)
+}
