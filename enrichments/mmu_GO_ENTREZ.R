0e0f5e3e4d34d026292cc2986553416cdea92965
Daniel Osorio <dosorio@users.noreply.github.com>
2019-08-02 09:24:41 -0500

    Update mmu_GO_ENTREZ.R

--- enrichments/mmu_GO_ENTREZ.R
+++ enrichments/mmu_GO_ENTREZ.R
@@ -3,6 +3,6 @@ mmu_GO_ENTREZ <- function(X){
   require(AnnotationHub)
   db <- AnnotationHub()
   mmu <- db[["AH70573"]]
-  out <- clusterProfiler::enrichGO(gene = X, keyType = "ENTREZID", OrgDb = mmu)
+  out <- clusterProfiler::enrichGO(gene = X, keyType = "ENTREZID", OrgDb = mmu, ont = "ALL")
   as.data.frame(out)
 }
