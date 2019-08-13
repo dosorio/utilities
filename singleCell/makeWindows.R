5ba3c343c73938960503add5315d99a9a6d1353c
Daniel Osorio <dosorio@users.noreply.github.com>
2019-08-13 08:35:19 -0500

    Update makeWindows.R

--- singleCell/makeWindows.R
+++ singleCell/makeWindows.R
@@ -1,7 +1,10 @@
-# W is the number of desired Windows
-makeWindows <- function(X, W=10){
-  nCol <- ncol(X)
-  W <- W+1
-  O <- nCol/W
-  zoo::rollapply(seq_len(nCol), W, by=O, c)
+makeWindows <- function(cMatrix, nWindows, ovlpProportion = 0.5){
+  nCol <- ncol(cMatrix)
+  makeM <- function(X){
+    O <- (X * ovlpProportion)
+    M <- zoo::rollapply(seq_len(nCol), X, by=O, c)
+    abs(dim(M)[1] - nWindows)
+  }
+  Z <- optimize(f = makeM, interval = seq_len(nCol))
+  zoo::rollapply(seq_len(nCol), Z$minimum, by=Z$minimum*ovlpProportion, c)
 }
