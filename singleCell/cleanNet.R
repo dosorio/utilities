4481e968f381b16827a8861783a4b6eaf15fe6f9
Daniel Osorio <dosorio@users.noreply.github.com>
2019-08-22 13:18:57 -0500

    Update cleanNet.R

--- singleCell/cleanNet.R
+++ singleCell/cleanNet.R
@@ -1,8 +1,8 @@
 cleanNet <- function(X){
-  X <- as.matrix(X[,])
-  X <- reshape2::melt(X)
-  colnames(X) <- c("from", "to", "value")
-  X <- X[X[,3] > 0,]
-  X <- igraph::graph_from_data_frame(X)
-  return(X)
-}
+    X <- as.matrix(X[,])
+    X <- reshape2::melt(X)
+    colnames(X) <- c("from", "to", "weight")
+    X <- X[abs(X[,3]) > 0,]
+    X <- igraph::graph_from_data_frame(X)
+    return(X)
+  }
