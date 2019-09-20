5e2e3d26444b69bb68d07323d6c009869d0f14a5
Daniel Osorio <dosorio@users.noreply.github.com>
2019-09-19 21:49:44 -0500

    Update hsa_KEGG_ENTREZ.R

--- enrichments/hsa_KEGG_ENTREZ.R
+++ enrichments/hsa_KEGG_ENTREZ.R
@@ -1,6 +1,6 @@
-hsa_KEGG_ENTREZ <- function(X){
+hsa_KEGG_ENTREZ <- function(X, ...){
   require(clusterProfiler)
   require(AnnotationHub)
-  eOut <- clusterProfiler::enrichKEGG(gene = X,organism = "hsa")
+  eOut <- clusterProfiler::enrichKEGG(gene = X,organism = "hsa", ...)
   as.data.frame(eOut)
 }
