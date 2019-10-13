cb2c0b8e34bd9e2b9a7e36b5b96cdd39e039390c
Daniel Osorio <dosorio@users.noreply.github.com>
2019-10-13 10:55:49 -0500

    Update gSet_HSA_KEGG_SYMBOL.R

--- enrichments/gSet_HSA_KEGG_SYMBOL.R
+++ enrichments/gSet_HSA_KEGG_SYMBOL.R
@@ -1,5 +1,6 @@
 gSet_HSA_KEGG_SYMBOL <- function(){
   source('https://raw.githubusercontent.com/dosorio/utilities/master/enrichments/gSets_KEGG.R')
+  source('https://raw.githubusercontent.com/dosorio/utilities/master/idConvert/hsa_ENTREZ2SYMBOL.R')
   hsaKEGG <- gSets_KEGG("hsa")
   hsaKEGG <- lapply(hsaKEGG, function(X){
     hsa_ENTREZ2SYMBOL(X)[,2]
