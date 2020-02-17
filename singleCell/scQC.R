243032ef6ec5f3451bf01196477851ac1af4e3bb
Daniel Osorio <dosorio@users.noreply.github.com>
2020-02-17 14:57:45 -0600

    Update scQC.R

--- singleCell/scQC.R
+++ singleCell/scQC.R
@@ -1,10 +1,27 @@
-scQC <- function(X){
-  lSize <- apply(X,2,sum)
-  lLimits <- quantile(lSize, c(0.15,0.95))
-  mtCounts <- apply(X[grepl("^MT",toupper(rownames(X))),,drop=FALSE],2,sum)
-  mtRate <- mtCounts/lSize
-  #X <- t(t(X)/lSize)*1e6
-  X <- X[,lSize > lLimits[1] & lSize < lLimits[2] & mtRate < 0.1]
-  X <- X[apply(X != 0, 1 ,mean) > 0.1,]
+scQC <- function(X, mtThreshold = 0.1){
+  require(Matrix)
+  if(class(X) == 'Seurat'){
+    countMatrix <- X@assays$RNA@counts
+  } else {
+    countMatrix <- X
+  }
+  librarySize <- colSums(countMatrix)
+  mtCounts <- colSums(countMatrix[grep('^MT-',toupper(rownames(countMatrix))),])
+  nGenes <- colSums(countMatrix != 0)
+  mtProportion <- mtCounts/librarySize
+  
+  genesLM <- lm(nGenes~librarySize)     
+  mtLM <- lm(mtCounts~librarySize)     
+  
+  genesLM <- as.data.frame(predict(genesLM, data.frame(librarySize), interval = 'prediction'))
+  mtLM <- as.data.frame(predict(mtLM, data.frame(librarySize), interval = 'prediction'))
+  
+  selectedCells <- mtCounts > mtLM$lwr & mtCounts < mtLM$upr & nGenes > genesLM$lwr & nGenes < genesLM$upr & mtProportion <= mtThreshold & librarySize < 2 * mean(librarySize)
+  selectedCells <- colnames(countMatrix)[selectedCells]
+  if(class(X) == 'Seurat'){
+    X <- subset(X, cells = selectedCells)
+  } else {
+    X <- countMatrix[,selectedCells]
+  }
   return(X)
 }
