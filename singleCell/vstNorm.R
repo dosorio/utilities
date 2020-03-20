24e64e11429d5139dd1a0fd39c6e22292fe67c95
Daniel Osorio <dosorio@users.noreply.github.com>
2020-03-20 13:52:48 -0500

    Update vstNorm.R

--- singleCell/vstNorm.R
+++ singleCell/vstNorm.R
@@ -2,7 +2,7 @@ vstNorm <- function(X){
   X <- as.matrix(X)
   X[X == 0] <- NA
   loggeomeans <- rowMeans(log(X))
-  sf <- apply(counts, 2, function(cnts){
+  sf <- apply(X, 2, function(cnts){
     exp(stats::median((log(cnts) - loggeomeans)[is.finite(loggeomeans) & cnts > 0], na.rm = TRUE))
   })
   X <- t(t(X)/sf)
