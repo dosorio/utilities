0c54798a24776f96db3dfc67485788a2c8051bd0
Daniel Osorio <dosorio@users.noreply.github.com>
2020-03-25 13:22:03 -0500

    Create plotKO.R

---
+++ singleCell/plotKO.R
@@ -0,0 +1,136 @@
+plotKO <- function(X, gKO, q = 0.99, annotate = FALSE, nCategories = 20, fdrThreshold = 0.05){
+  library(enrichR)
+  library(igraph)
+  gList <- X$diffRegulation$gene[X$diffRegulation$distance > 1e-10 & X$diffRegulation$p.adj < 0.05]
+  if(length(gList) > 0){
+    sCluster <- as.matrix(X$WT[gList,gList])
+    koInfo <- sCluster[gKO,]
+    
+    gList <- gList[!grepl('^mt-|^Rpl|^Rps',gList, ignore.case = TRUE)]
+    sCluster[abs(sCluster) <= quantile(abs(sCluster), q)] <- 0
+    sCluster[gKO,] <- koInfo
+    diag(sCluster) <- 0
+    sCluster <-  reshape2::melt(as.matrix(sCluster))
+    colnames(sCluster) <- c('from', 'to', 'W')
+    sCluster <- sCluster[sCluster$W != 0,]
+    netPlot <- graph_from_data_frame(sCluster, directed = TRUE)
+    dPlot <- centr_degree(netPlot)$res
+    W <- rep(1,nrow(sCluster))
+    sG   <- (names(V(netPlot))[dPlot > 1])[-1]
+    W[sCluster$from %in% sG] <- 0.2
+    W[sCluster$to %in% sG] <- 0.2
+    W[sCluster$from %in% gKO] <- 1
+    W[sCluster$from %in% gKO & sCluster$to %in% sG] <- 0.8
+    set.seed(1)
+    layPlot <- layout_with_fr(netPlot, weights = W)
+    dPlot <- (dPlot/max(dPlot))*20
+    
+    
+    if(isTRUE(annotate)){
+      enrichFunction <- function(X, fdrThreshold = fdrThreshold){
+        E <- enrichr(X, c('KEGG_2019_Human', 'GO_Biological_Process_2018', 'BioPlanet_2019', 'WikiPathways_2019_Human', 'Reactome_2016'))
+        E <- do.call(rbind.data.frame, E)
+        E <- E[E$Adjusted.P.value < fdrThreshold,]
+        E <- E[order(E$Adjusted.P.value),]
+        E$Term <- unlist(lapply(strsplit(E$Term,''), function(X){
+          X[1] <- toupper(X[1])
+          X <- paste0(X,collapse = '')
+          X <- gsub('\\([[:print:]]+\\)|Homo[[:print:]]+|WP[[:digit:]]+','',X)
+          X <- gsub("'s",'',X)
+          X <- unlist(strsplit(X,','))[1]
+          X <- gsub('[[:blank:]]$','',X)
+          return(X)
+        }))
+        selectedSet <- rep(FALSE, nrow(E))
+        for(i in seq_len(nrow(E))){
+          if(i == 1){
+            selectedSet[i] <- TRUE
+          } else {
+            A <- unique(unlist(strsplit(E[which(selectedSet[seq_len(i)]),'Genes'], ';')))
+            B <- unlist(strsplit(E[i,'Genes'], ';'))
+            selectedSet[i] <- !all(B %in% A)
+          }
+        }
+        gSets <- table(toupper(E$Term))
+        gSets <- names(gSets[gSets > 1])
+        for(i in gSets){
+          selectedSet[which(toupper(E$Term) %in% i)[-1]] <- FALSE
+        }
+        E <- E[selectedSet,]
+        if(nrow(E) > nCategories){
+          E <- E[seq_len(nCategories),]  
+        }
+        return(E)
+      }
+      E <- enrichFunction(gList, fdrThreshold)
+      if(isTRUE(nrow(E) > 0)){
+        tPlot <- strsplit(E$Genes, ';')
+        pPlot <- matrix(0,nrow = length(V(netPlot)), ncol = nrow(E))
+        rownames(pPlot) <- toupper(names(V(netPlot)))
+        for(i in seq_along(tPlot)){
+          pPlot[unlist(tPlot[i]),i] <- 1
+        }
+        pPlot <- lapply(seq_len(nrow(pPlot)), function(X){as.vector(pPlot[X,])})
+        names(pPlot) <- names(V(netPlot))
+        tPlot <- unique(unlist(tPlot))
+        eGenes <- toupper(names(V(netPlot))) %in% tPlot
+        vColor <- rgb(0,188/255,1,0.3)
+        par(mar=c(4,0,0,0), xpd = TRUE)
+        plot(netPlot,
+             layout = layPlot, 
+             edge.arrow.size=.2,
+             vertex.label.color="black", 
+             vertex.shape = ifelse(eGenes,'pie','circle'),
+             vertex.pie = pPlot,
+             vertex.size = 10+dPlot, 
+             vertex.pie.color=list(hcl.colors(nrow(E), palette = 'Zissou 1', alpha = 0.7)),
+             vertex.label.family="Times", 
+             vertex.label.font=ifelse(eGenes,2,1),
+             edge.color = ifelse(E(netPlot)$W > 0, 'red', 'blue'),
+             edge.curved = ifelse(W == 0.2, 0, 0.1),
+             vertex.color = vColor, 
+             vertex.frame.color = NA)
+        sigLevel <- formatC(E$Adjusted.P.value, digits = 2, format = 'g', width = 0, drop0trailing = TRUE)
+        gSetNames <- lengths(strsplit(E$Genes, ';'))
+        gSetNames <- paste0('(', gSetNames,') ', E$Term, ' FDR = ', sigLevel)
+        legend(x = -1.05, y = -1.05, legend = gSetNames, bty = 'n', ncol = 2, cex = 1, col = hcl.colors(nrow(E), palette = 'Zissou 1'), pch = 16)
+      } else {
+        par(mar=c(0,0,0,0))
+        plot(netPlot,
+             layout = layPlot, 
+             edge.arrow.size=.2,
+             vertex.label.color="black",
+             vertex.size = 10+dPlot, 
+             vertex.label.family="Times", 
+             edge.color = ifelse(E(netPlot)$W > 0, 'red', 'blue'),
+             edge.curved = ifelse(W == 0.2, 0, 0.1),
+             vertex.color = rgb(0,188/255,1,0.3), 
+             vertex.frame.color = NA)
+      }
+    } else {
+      par(mar=c(0,0,0,0))
+      plot(netPlot,
+           layout = layPlot, 
+           edge.arrow.size=.2,
+           vertex.label.color="black",
+           vertex.size = 10+dPlot, 
+           vertex.label.family="Times", 
+           edge.color = ifelse(E(netPlot)$W > 0, 'red', 'blue'),
+           edge.curved = ifelse(W == 0.2, 0, 0.1),
+           vertex.color = rgb(0,188/255,1,0.3), 
+           vertex.frame.color = NA)
+    }
+  } else {
+    netPlot <- matrix(0,1,1)
+    rownames(netPlot) <- colnames(netPlot) <- gKO
+    netPlot <- graph_from_adjacency_matrix(netPlot)
+    par(mar=c(0,0,0,0))
+    plot(netPlot,
+         edge.arrow.size=.2,
+         vertex.size = 50,
+         vertex.label.color="black",
+         vertex.label.family="Times", 
+         vertex.color = rgb(0,188/255,1,0.3), 
+         vertex.frame.color = NA)
+  }
+}
