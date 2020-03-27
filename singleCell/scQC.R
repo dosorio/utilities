27e614ca9851581d83af0107d3eec689f55f9677
Daniel Osorio <dosorio@users.noreply.github.com>
2020-03-27 11:33:28 -0500

    Update scQC.R

--- singleCell/scQC.R
+++ singleCell/scQC.R
@@ -1,22 +1,27 @@
-scQC <- function(X, mtThreshold = 0.1){
-  require(Matrix)
+scQC <- function(X, mtThreshold = 0.1, minLSize = 1000){
   if(class(X) == 'Seurat'){
     countMatrix <- X@assays$RNA@counts
   } else {
     countMatrix <- X
   }
   librarySize <- colSums(countMatrix)
-  mtCounts <- colSums(countMatrix[grep('^MT-',toupper(rownames(countMatrix))),])
+  countMatrix <- countMatrix[,librarySize >= minLSize]
+  librarySize <- colSums(countMatrix)
+  mtGenes <- grep('^MT-',toupper(rownames(countMatrix)))
   nGenes <- colSums(countMatrix != 0)
-  mtProportion <- mtCounts/librarySize
-  
-  genesLM <- lm(nGenes~librarySize)     
-  mtLM <- lm(mtCounts~librarySize)     
-  
+
+  genesLM <- lm(nGenes~librarySize)
   genesLM <- as.data.frame(predict(genesLM, data.frame(librarySize), interval = 'prediction'))
-  mtLM <- as.data.frame(predict(mtLM, data.frame(librarySize), interval = 'prediction'))
-  
-  selectedCells <- mtCounts > mtLM$lwr & mtCounts < mtLM$upr & nGenes > genesLM$lwr & nGenes < genesLM$upr & mtProportion <= mtThreshold & librarySize < 2 * mean(librarySize)
+
+  if(isTRUE(length(mtGenes) > 0)){
+    mtCounts <- colSums(countMatrix[grep('^MT-',toupper(rownames(countMatrix))),])
+    mtProportion <- mtCounts/librarySize
+    mtLM <- lm(mtCounts~librarySize)
+    mtLM <- as.data.frame(predict(mtLM, data.frame(librarySize), interval = 'prediction'))
+    selectedCells <- mtCounts > mtLM$lwr & mtCounts < mtLM$upr & nGenes > genesLM$lwr & nGenes < genesLM$upr & mtProportion <= mtThreshold & librarySize < 2 * mean(librarySize)
+  } else {
+    selectedCells <- nGenes > genesLM$lwr & nGenes < genesLM$upr & librarySize < 2 * mean(librarySize)
+  }
   selectedCells <- colnames(countMatrix)[selectedCells]
   if(class(X) == 'Seurat'){
     X <- subset(X, cells = selectedCells)
