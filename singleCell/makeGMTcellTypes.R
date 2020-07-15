1260a1b2455729f6c6d5cdc4d994ea0bc2b0c07c
Daniel Osorio <dcosorioh@tamu.edu>
2020-07-14 20:49:16 -0500

    Update makeGMTcellTypes.R

--- singleCell/makeGMTcellTypes.R
+++ singleCell/makeGMTcellTypes.R
@@ -1,4 +1,5 @@
 library(clustermole)
+library(fgsea)
 
 mmPanglao <- clustermole_markers(species = 'mm')
 mmPanglao <- mmPanglao[mmPanglao$species %in% 'Mouse',]
@@ -8,3 +9,13 @@ ctList <- sapply(ctList, function(X){
   paste0(c(X, unique(mmPanglao$gene[mmPanglao$celltype %in% X])), collapse = '\t')
 })
 writeLines(ctList, 'mmPanglaoDB.gmt')
+
+
+hsaPanglao <- clustermole_markers(species = 'hs')
+hsaPanglao <- hsaPanglao[hsaPanglao$species %in% 'Human',]
+hsaPanglao <- hsaPanglao[hsaPanglao$db %in% 'PanglaoDB',]
+ctList <- unique(hsaPanglao$celltype)
+ctList <- sapply(ctList, function(X){
+  paste0(c(X, unique(hsaPanglao$gene[hsaPanglao$celltype %in% X])), collapse = '\t')
+})
+writeLines(ctList, 'hsaPanglaoDB.gmt')
