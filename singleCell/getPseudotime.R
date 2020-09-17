1f4e2c98cc2d2adb961dfb39b4be9bbfabff3174
Daniel Osorio <dosorio@users.noreply.github.com>
2020-09-17 09:47:02 -0500

    Update getPseudotime.R

--- singleCell/getPseudotime.R
+++ singleCell/getPseudotime.R
@@ -1,5 +1,15 @@
-getPseudoTime <- function(cMatrix){
+getPseudoTime <- function(cMatrix, simplified = TRUE, nDim = 100){
   cMatrix <- cMatrix[rowSums(cMatrix) > 0,]
+  if(isTRUE(simplified)){
+    require(RSpectra)
+    require(Matrix)
+    nMatrix <- log1p(t(t(cMatrix)/ColSums(cMatrix)) * 1e4)
+    nMatrix <- scale(t(nMatrix))
+    nMatrix <- t(svds(nMatrix, nDim)$v)
+    colnames(nMatrix) <- colnames(cMatrix)
+    rownames(nMatrix) <- paste0('g', seq_len(nDim))
+    cMatrix <- nMatrix
+  }
   require(monocle)
   fd <- data.frame('gene_short_name' = rownames(cMatrix))
   rownames(fd) <- rownames(cMatrix)
