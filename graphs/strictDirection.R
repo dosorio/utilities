ef5d7df1c321a2262244a4000cc313746064324f
Daniel Osorio <dosorio@users.noreply.github.com>
2020-12-14 12:40:53 -0600

    Update strictDirection.R

--- graphs/strictDirection.R
+++ graphs/strictDirection.R
@@ -1,10 +1,6 @@
-strictDirection <- function(X){
-  lT <- X[lower.tri(X)]
-  uT <- t(X)[lower.tri(X)]
-  all(lT == uT)
-  lT[abs(lT) < abs(uT)] <- 0
-  uT[abs(uT) < abs(lT)] <- 0
-  X[lower.tri(X)] <- lT
-  X[upper.tri(X)] <- uT
-  return(X)
+strictDirection <- function(X, lambda = 1){
+  S <- X
+  S[abs(S) < abs(t(S))] <- 0
+  O <- (((1-lambda) * X) + (lambda * S))
+  return(O)
 }
