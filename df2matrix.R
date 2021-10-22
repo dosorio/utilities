f5bd333bc948f79eba2e8f7cdabe022ef0e3bcbd
Daniel Osorio <dosorio@users.noreply.github.com>
2021-10-22 09:11:59 -0500

    Matrix added

--- df2matrix.R
+++ df2matrix.R
@@ -1,8 +1,9 @@
 df2matrix <- function(df, columns, rows, values){
+  require(Matrix)
   df <- df[,c(columns, rows, values)]
   sList <- unique(df[,columns])
   fList <- unique(df[,rows])
-  oMatrix <- matrix(0, nrow = length(fList), ncol = length(sList))
+  oMatrix <- Matrix(matrix(0, nrow = length(fList), ncol = length(sList)))
   colnames(oMatrix) <- sList
   rownames(oMatrix) <- fList
   O <- pbapply::pbapply(df,1,function(X){
