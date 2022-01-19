ed700f51108280d46213c876321cf003a770dca2
Daniel Osorio <dosorio@users.noreply.github.com>
2022-01-19 15:52:59 -0600

    Update networkScoring.R

--- graphs/networkScoring.R
+++ graphs/networkScoring.R
@@ -9,25 +9,29 @@ networkScoring <- function(rNetwork, geneSets){
   geneNames <- names(V(rNetwork))
   
   # The score is computed using the averages shorted paths between vertices multiplied by the 
-  # proportion observed of each gene set and scaled accordingly to the promiscuity of the genes
-  # included
+  # specificity and the proportion observed of each gene set. 
   geneSetScores <- lapply(geneSets, function(gSet){
     setSize <- length(gSet)
     gSet <- gSet[gSet %in% geneNames]
     obsProportion <- length(gSet)/setSize
     setSpecificity <- mean(geneSpecificty[gSet])
-    setScore <- (mean_distance(induced_subgraph(rNetwork, gSet), directed = FALSE, unconnected = TRUE) * obsProportion) / setSpecificity
+    setDistances <- induced_subgraph(rNetwork, gSet)
+    setDistances <- distances(setDistances)
+    setDistances[!is.finite(setDistances)] <- 0
+    gmSetDistances <- exp(mean(log(setDistances[setDistances > 0])))
+    setScore <- (gmSetDistances * obsProportion) / setSpecificity
     return(setScore)
   })
   
   # Scores are normalized using the BOX-COX normalization method
   geneSetScores <- unlist(geneSetScores)
   geneSetScores[!is.finite(geneSetScores)] <- 0
-  BC <- MASS::boxcox(geneSetScores[geneSetScores >  0]~1, plotit = FALSE)
+  
+  BC <- MASS::boxcox(geneSetScores[geneSetScores >  0] ~ 1, plotit = FALSE)
   BC <- 1 + abs(BC$x[which.max(BC$y)])
   geneSetScores <- geneSetScores ^ BC
   
-  # Scores are standardized
+   # Scores are standardized
   geneSetScores <- scale(geneSetScores)[,1]
   
   # Return
