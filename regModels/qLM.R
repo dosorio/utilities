83ecc11990bac1b13267ff7f982a47aafac73d22
Daniel Osorio <dosorio@users.noreply.github.com>
2022-08-02 08:41:47 -0500

    Create qLM.R

---
+++ regModels/qLM.R
@@ -0,0 +1,10 @@
+qLM <- function(X, y){
+  X <- cbind(1,X)
+  m <- solve(t(X) %*% X) %*% t(X) %*% y
+  yHat <- (X %*% m)
+  MSM <- rowSums((t(yHat) - colMeans(y))^2)/(p-1)
+  MSE <- colSums((y - yHat)^2)/(n-p)
+  P <- pf(MSM/MSE, df1 = p-1, df2 = n-p, lower.tail = FALSE)
+  O <- data.frame(beta = t(m)[,1],P)
+  return(O)
+}
