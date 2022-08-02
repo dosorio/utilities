824a26d8e68fa083d0f68ec184bdc409e53e4150
Daniel Osorio <dosorio@users.noreply.github.com>
2022-08-02 08:42:18 -0500

    Update qLM.R

--- regModels/qLM.R
+++ regModels/qLM.R
@@ -1,7 +1,7 @@
 qLM <- function(X, y){
   X <- cbind(1,X)
   m <- solve(t(X) %*% X) %*% t(X) %*% y
-  yHat <- (X %*% m)
+  yHat <- X %*% m
   MSM <- rowSums((t(yHat) - colMeans(y))^2)/(p-1)
   MSE <- colSums((y - yHat)^2)/(n-p)
   P <- pf(MSM/MSE, df1 = p-1, df2 = n-p, lower.tail = FALSE)
